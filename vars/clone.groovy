6cb0ab04b4e0e96e453aa2970de746476fd0a3b4
devopspractice8 <ayubshaikh9226@gmail.com>
2025-07-06 15:45:50 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,7 @@
-def call(String url,String branch){
-  echo "This is Cloning the Code"
-  git url: "$(url)", branch: "$(branch)"
-  echo "Code Cloning Successful"
+// Jenkins_Shared_Library/vars/clone.groovy
+
+def call(String url, String branch) {
+  echo "📥 This is Cloning the Code"
+  git url: url, branch: branch
+  echo "✅ Code Cloning Successful"
 }
